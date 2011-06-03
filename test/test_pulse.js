5870a58b127b6112609a7b348b0f7867057611d7
Shawn Wilsher <me@shawnwilsher.com>
2011-06-03 18:50:59 +0000

    Use sinon to create spys/stubs instead of doing it on my own.

--- test/test_pulse.js
+++ test/test_pulse.js
@@ -1,16 +1,8 @@
 var pulse = require("../pulse.js");
+var testCase = require("nodeunit").testCase;
+var sinon = require("sinon");
 var amqp = require("amqp");
 var events = require("events");
-
-// Before we do anything, we need to modify amqp.createConnection so that it
-// does not actually try to connect to pulse.
-gConnectionEvents = undefined;
-amqp.createConnection = function() {
-  var conn = new events.EventEmitter();
-  conn.end = function() { this.closed = true; };
-  return gConnectionEvents = conn;
-};
-
 
 ////////////////////////////////////////////////////////////////////////////////
 //// Constants
@@ -26,53 +18,75 @@ var kTypes = [
 ////////////////////////////////////////////////////////////////////////////////
 //// Test Functions
 
-exports.test_no_work_until_listener = function(test) {
-  test.expect(2);
-  var c = new pulse.createConsumer("test", "test");
-  test.strictEqual(gConnectionEvents, undefined);
+module.exports = testCase({
+  setUp: function(callback)
+  {
+    this.createConnectionStub = sinon.stub(amqp, "createConnection");
 
-  c.on("message", function() {});
-  test.ok(gConnectionEvents);
-  test.done();
-};
+    this.connection = new events.EventEmitter();
+    this.connection.end = sinon.spy();
+    this.createConnectionStub.returns(this.connection);
 
-exports.test_consumer_prototype = function(test) {
-  test.expect(kTypes.length);
-  kTypes.forEach(function(type) {
-    var c = new pulse.createConsumer(type, type);
-    test.ok(c instanceof events.EventEmitter);
-  });
-  test.done();
-};
+    callback();
+  },
+  tearDown: function(callback)
+  {
+    this.createConnectionStub.restore();
+    callback();
+  },
 
-exports.test_error_reporting = function(test) {
-  test.expect(1);
-  var c = new pulse.createConsumer("test", "test");
-  var kTestError = { name: "test error" };
-  c.on("error", function(e) {
-    test.strictEqual(e, kTestError);
+  test_no_work_until_listener: function(test)
+  {
+    test.expect(2);
+    var c = new pulse.createConsumer("test", "test");
+    test.equals(this.createConnectionStub.callCount, 0);
+
+    c.on("message", function() {});
+    test.ok(this.createConnectionStub.calledOnce);
     test.done();
-  });
-  gConnectionEvents.emit("error", kTestError);
-};
+  },
 
-exports.test_close = function(test) {
-  test.expect(6);
+  test_consumer_prototype: function(test)
+  {
+    test.expect(kTypes.length);
+    kTypes.forEach(function(type) {
+      var c = new pulse.createConsumer(type, type);
+      test.ok(c instanceof events.EventEmitter);
+    });
+    test.done();
+  },
 
-  // Closing an unopened connection should not throw.
-  var c = new pulse.createConsumer("test", "test");
-  test.doesNotThrow(function() { c.close(); });
-  test.strictEqual(gConnectionEvents.closed, undefined);
+  test_error_reporting: function(test)
+  {
+    test.expect(1);
+    var c = new pulse.createConsumer("test", "test");
+    var kTestError = { name: "test error" };
+    c.on("error", function(e) {
+      test.strictEqual(e, kTestError);
+      test.done();
+    });
+    this.connection.emit("error", kTestError);
+  },
 
-  // Closing an opened connection should not throw and clear out _server.
-  c = new pulse.createConsumer("test", "test");
-  c.on("message", function() {});
-  test.doesNotThrow(function() { c.close(); });
-  test.ok(gConnectionEvents.closed);
-  test.strictEqual(c._server, undefined);
+  test_close: function(test)
+  {
+    test.expect(6);
 
-  // Closing an already closed connection should throw though.
-  test.throws(function() { c.close(); });
+    // Closing an unopened connection should not throw.
+    var c = new pulse.createConsumer("test", "test");
+    test.doesNotThrow(function() { c.close(); });
+    test.equal(this.connection.end.callCount, 0);
 
-  test.done();
-};
+    // Closing an opened connection should not throw and clear out _server.
+    c = new pulse.createConsumer("test", "test");
+    c.on("message", function() {});
+    test.doesNotThrow(function() { c.close(); });
+    test.ok(this.connection.end.calledOnce);
+    test.strictEqual(c._server, undefined);
+
+    // Closing an already closed connection should throw though.
+    test.throws(function() { c.close(); });
+
+    test.done();
+  },
+});
